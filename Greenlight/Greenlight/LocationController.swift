9967b818d49c8a9bf3192b23df9e2fe0e40061ae
LilyxxLin <lilyxxlin@gmail.com>
2021-03-06 12:36:02 +0800

    adding in repository

---
+++ Greenlight/Greenlight/LocationController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Greenlight
+//
+//  Created by Lily Lin on 3/6/21.
+//
+
+import UIKit
+
+class LocationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
